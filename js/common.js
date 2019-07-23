0661dc1e7ea965ba5894791c1673e19f7e3ab7c2
Michal Mucha <michal.mucha@synapse.com>
2019-07-23 19:13:30 +0200

    main page init commit

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+  $('.friends-carousel').owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+  });
+});
