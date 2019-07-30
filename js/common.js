883dec55b8d2b444245861adce835a77234e1e67
Michal Mucha <michal.mucha@synapse.com>
2019-07-30 23:22:11 +0200

    style up navigation, events section

--- js/common.js
+++ js/common.js
@@ -5,6 +5,18 @@ $( document ).ready(function() {
     margin:10,
     autoplay:true,
     autoplayTimeout:3000,
-    autoplayHoverPause:true
+    autoplayHoverPause:true,
+    dots: false,
+    responsive:{
+      0:{
+          items:1,
+      },
+      600:{
+          items:3,
+      },
+      1000:{
+          items:6
+      }
+  }
   });
 });
